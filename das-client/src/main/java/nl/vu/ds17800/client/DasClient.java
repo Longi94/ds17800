cee95a0d3b75281c11cef03d7e139b7ef8f106ef
Long Tran <lngtrn94@gmail.com>
2017-11-16 14:08:21 +0100

    init

---
+++ das-client/src/main/java/nl/vu/ds17800/client/DasClient.java
@@ -0,0 +1,11 @@
+package nl.vu.ds17800.client;
+
+/**
+ * @author lngtr
+ * @since 2017-11-16
+ */
+public class DasClient {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
---
+++ das-server/src/main/java/nl/vu/ds17800/server/DasServer.java
@@ -0,0 +1,11 @@
+package nl.vu.ds17800.server;
+
+/**
+ * @author lngtr
+ * @since 2017-11-16
+ */
+public class DasServer {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
