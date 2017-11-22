7cac481a2217125a4f0d807f32c2581663beb79e
Saidgani Musaev <cpu808694@gmail.com>
2017-11-22 20:48:40 +0100

    Separate worker for responding; Socket pool

--- das-core/src/main/java/nl/vu/ds17800/core/networking/CommunicationImpl.java
+++ das-core/src/main/java/nl/vu/ds17800/core/networking/CommunicationImpl.java
@@ -1,13 +1,17 @@
 package nl.vu.ds17800.core.networking;
 
 import nl.vu.ds17800.core.networking.response.Message;
+import nl.vu.ds17800.core.networking.response.MessageContainer;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,16 +20,34 @@ import java.util.Map;
  */
 public class CommunicationImpl implements Communication {
     static private final int DSPORT = 666;
+    static private final String HEARTBEATING = "heartbeating";
     private IncomingHandler incomeHandler;
+    private LinkedList<MessageContainer> incomeBuffer;
+    private HashMap<String, Socket> socketPool;
 
     public CommunicationImpl(IncomingHandler handler){
+        incomeBuffer = new LinkedList<MessageContainer>();
         incomeHandler = handler;
+        socketPool = new HashMap<String, Socket>();
     }
 
     public Message sendMessage(Message message, String inetAddress) throws IOException, ClassNotFoundException {
-        Socket socket = new Socket(InetAddress.getByName(inetAddress), DSPORT);
+        Socket socket;
+        socket = socketPool.get(inetAddress);
+        if(socket == null)
+            socket = new Socket(InetAddress.getByName(inetAddress), DSPORT);
+        Message testMessage = new Message();
+        testMessage.type = HEARTBEATING;
         ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream oin = new ObjectInputStream(socket.getInputStream());
+        try{
+            oout.writeObject(testMessage);
+        } catch (IOException e){
+            socket = new Socket(InetAddress.getByName(inetAddress), DSPORT);
+            oout = new ObjectOutputStream(socket.getOutputStream());
+            oin = new ObjectInputStream(socket.getInputStream());
+        }
+        socketPool.put(inetAddress, socket);
         oout.writeObject(message);
         return (Message)oin.readObject();
     }
@@ -40,15 +62,16 @@ public class CommunicationImpl implements Communication {
     }
 
     public void init(){
-        NetworkRouter routerThread = new NetworkRouter(incomeHandler);
+        NetworkRouter routerThread = new NetworkRouter(incomeBuffer);
+        Worker worker = new Worker(incomeHandler, incomeBuffer);
         routerThread.start();
+        worker.start();
     }
 
     private class NetworkRouter extends Thread{
-        private IncomingHandler messagesHandler;
-
-        public NetworkRouter(IncomingHandler handler){
-            messagesHandler = handler;
+        private LinkedList<MessageContainer> messagesBuffer;
+        public NetworkRouter(LinkedList<MessageContainer> list){
+            this.messagesBuffer = list;
         }
 
         public void run(){
@@ -58,13 +81,54 @@ public class CommunicationImpl implements Communication {
                 while(true){
                     Socket socket = ssocket.accept();
                     ObjectInputStream oin = new ObjectInputStream(socket.getInputStream());
-                    ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
                     Message message = (Message) oin.readObject();
-                    oout.writeObject(messagesHandler.handleMessage(message));
-                    socket.close();
+                    if(message.type == HEARTBEATING)
+                        continue;
+                    MessageContainer mContainer = new MessageContainer();
+                    mContainer.socket = socket;
+                    mContainer.message = message;
+                    synchronized (messagesBuffer){
+                        this.messagesBuffer.add(mContainer);
+                        this.messagesBuffer.notifyAll();
+                    }
                 }
             } catch (IOException e) {
             } catch (ClassNotFoundException e) {
+            }
+        }
+    }
+
+    private class Worker extends Thread{
+        private IncomingHandler messageshandler;
+        private LinkedList<MessageContainer> messagesBuffer;
+
+        Worker(IncomingHandler handler, LinkedList<MessageContainer> list){
+            this.messagesBuffer = list;
+            messageshandler = handler;
+        }
+
+        public void run(){
+            while(true){
+                MessageContainer mContainer = null;
+                try{
+                    synchronized (messagesBuffer){
+                        while(mContainer == null){
+                            mContainer = messagesBuffer.getFirst();
+                            messagesBuffer.wait();
+                        }
+                        messagesBuffer.removeFirst();
+                    }
+                    ObjectOutputStream oout = new ObjectOutputStream(mContainer.socket.getOutputStream());
+                    oout.writeObject(messageshandler.handleMessage(mContainer.message));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    continue;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         }
     }
