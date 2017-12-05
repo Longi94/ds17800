085e51a81a65ee322fb4c691154601e1dda131e2
Long Tran <lngtrn94@gmail.com>
2017-12-05 20:01:29 +0100

    update batch script

--- run-similation.bat
+++ run-similation.bat
@@ -4,20 +4,22 @@ start cmd /k java -jar das-server-%version%.jar 10100
 
 start cmd /k java -jar das-server-%version%.jar 10101
 
-start cmd /k java -jar das-server-%version%.jar 10102
+rem start cmd /k java -jar das-server-%version%.jar 10102
 
-start cmd /k java -jar das-server-%version%.jar 10103
+rem start cmd /k java -jar das-server-%version%.jar 10103
 
-start cmd /k java -jar das-server-%version%.jar 10104
+rem start cmd /k java -jar das-server-%version%.jar 10104
 
-timeout 5
+timeout 10
 
-for /l %%x in (1, 1, 10) do (
-    start cmd /k java -jar das-client-%version%.jar player
+for /l %%x in (1, 1, 5) do (
+    start cmd /k java -jar das-client-%version%.jar player 0
+    start cmd /k java -jar das-client-%version%.jar player 1
     timeout 1
 )
 
-for /l %%x in (1, 1, 5) do (
-    start cmd /k java -jar das-client-%version%.jar dragon
+for /l %%x in (1, 1, 1) do (
+    start cmd /k java -jar das-client-%version%.jar dragon 0
+    start cmd /k java -jar das-client-%version%.jar dragon 1
     timeout 1
 )
