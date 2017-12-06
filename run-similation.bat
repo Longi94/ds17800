25975fb3a6fee4daaa2b5229cae5c0890bbf2d78
Long Tran <lngtrn94@gmail.com>
2017-12-06 14:52:19 +0100

    start clients simultaneously

--- run-similation.bat
+++ run-similation.bat
@@ -1,39 +1,29 @@
 SET version=1.0.0-SNAPSHOT
 
-start cmd /k java -jar das-server-%version%.jar 10100 ^> server0.log
+start cmd /k java -jar das-server-%version%.jar 10100
 
-start cmd /k java -jar das-server-%version%.jar 10101 ^> server1.log
+start cmd /k java -jar das-server-%version%.jar 10101
 
-start cmd /k java -jar das-server-%version%.jar 10102 ^> server2.log
+start cmd /k java -jar das-server-%version%.jar 10102
 
-start cmd /k java -jar das-server-%version%.jar 10103 ^> server3.log
+start cmd /k java -jar das-server-%version%.jar 10103
 
-start cmd /k java -jar das-server-%version%.jar 10104 ^> server4.log
+start cmd /k java -jar das-server-%version%.jar 10104
 
 timeout 10
 
-for /l %%x in (1, 1, 2) do (
+for /l %%x in (1, 1, 4) do (
     start cmd /k java -jar das-client-%version%.jar player 0
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar player 1
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar player 2
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar player 3
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar player 4
-    timeout 5
 )
 
 for /l %%x in (1, 1, 1) do (
     start cmd /k java -jar das-client-%version%.jar dragon 0
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar dragon 1
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar dragon 2
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar dragon 3
-    timeout 5
     start cmd /k java -jar das-client-%version%.jar dragon 4
-    timeout 5
 )
